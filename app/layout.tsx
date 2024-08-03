a63cb0e84a509181ece1ad36898c01c6c8b0851b
KaterinUtkina <katerin.utkina@gmail.com>
2024-08-03 14:52:53 +0300

    init next app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {ReactNode} from "react";
 
 
 export const metadata: Metadata = {
@@ -10,11 +11,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
   );
 }
